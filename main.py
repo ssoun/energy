38106b9a09e8693a5fb84c3a67972af3a88f20b5
YeomJaemoo <125108268+YeomJaemoo@users.noreply.github.com>
2024-10-14 23:10:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
+# To install langchain_huggingface, run the following command:
+# !pip install langchain-huggingface
+
+# To install langchain_openai, run the following command:
+# !pip install langchain-openai
+
 import streamlit as st
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, UnstructuredPowerPointLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain_community.callbacks import get_openai_callback
 from langchain.memory import ConversationBufferMemory
-from langchain.memory import StreamlitChatMessageHistory
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.schema.messages import HumanMessage, AIMessage
 import tiktoken
 import json
 import base64
+import speech_recognition as sr
+import tempfile
 
 def main():
-    st.set_page_config(page_title="energy", page_icon="🌻")
-    st.image('energy.png')
-    st.title("_:red[에너지 학습 도우미 ]_ 🏫")
-    st.header("😶주의!이 챗봇은 참고용으로 사용하세요!", divider='rainbow')
-
+    st.set_page_config(page_title="에너지", page_icon="🌻")
+    st.image('knowhow.png')
+    st.title("_:red[에너지 학습 도움이]_ 🏫")
+    st.header("😶주의! 이 창바드는 참고용으로 사용하세요!", divider='rainbow')
+    
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
@@ -29,12 +37,18 @@ def main():
     if "processComplete" not in st.session_state:
         st.session_state.processComplete = None
 
+    if "voice_input" not in st.session_state:
+        st.session_state.voice_input = ""
+
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = [{"role": "assistant", "content": "에너지 학습에 대해 문자보세요!😊"}]
+
     with st.sidebar:
         folder_path = Path()
         openai_api_key = st.secrets["OPENAI_API_KEY"]
         model_name = 'gpt-4o-mini'
         
-        st.text("아래의 'Process'를 누르고\n아래 채팅창이 활성화 될 때까지\n잠시 기다려주세요!🙂🙂🙂")
+        st.text("아래의 'Process'를 누르고\n아래 채팅창이 활성화 될 때까지\n잠시 기다리세요!😊😊😊")
         process = st.button("Process", key="process_button")
         
         if process:
@@ -43,6 +57,24 @@ def main():
             vectorstore = get_vectorstore(text_chunks)
             st.session_state.conversation = get_conversation_chain(vectorstore, openai_api_key, model_name)
             st.session_state.processComplete = True
+
+        audio_value = st.experimental_audio_input("음성 메시지를 녹음하세요.")
+
+        if audio_value:
+            with st.spinner("음성을 인식하는 중..."):
+                recognizer = sr.Recognizer()
+                try:
+                    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_audio_file:
+                        temp_audio_file.write(audio_value.getvalue())
+                        with sr.AudioFile(temp_audio_file.name) as source:
+                            audio = recognizer.record(source)
+                            st.session_state.voice_input = recognizer.recognize_google(audio, language='ko-KR')
+                except sr.UnknownValueError:
+                    st.warning("음성을 인식하지 못했습니다. 다시 시도하세요!")
+                except sr.RequestError:
+                    st.warning("서버와의 연결에 문제가 있습니다. 다시 시도하세요!")
+                except OSError:
+                    st.error("오디오 파일을 처리하는 데 문제가 발생했습니다. 다시 시도하세요.")
 
         save_button = st.button("대화 저장", key="save_button")
         if save_button:
@@ -54,41 +86,35 @@ def main():
         clear_button = st.button("대화 내용 삭제", key="clear_button")
         if clear_button:
             st.session_state.chat_history = []
-            st.session_state.messages = [{"role": "assistant", "content": "에너지 학습에 대해 물어보세요!😊"}]
-            st.experimental_rerun()  # 화면을 다시 로드하여 대화 내용을 초기화
+            st.session_state.messages = [{"role": "assistant", "content": "에너지 학습에 대해 문자보세요!😊"}]
+            st.experimental_set_query_params()  # 화면을 다시 로드하여 대화 내용을 초기화
 
-    if 'messages' not in st.session_state:
-        st.session_state['messages'] = [{"role": "assistant", 
-                                         "content": "에너지 학습에 대해 물어보세요!😊"}]
+    if st.session_state.voice_input:
+        query = st.session_state.voice_input
+        st.session_state.voice_input = ""
+    else:
+        query = st.chat_input("질문을 입력해주세요.")
 
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+    if query:
+        st.session_state.messages.insert(0, {"role": "user", "content": query})
+        chain = st.session_state.conversation
+        with st.spinner("생각 중..."):
+            result = chain({"question": query})
+            with get_openai_callback() as cb:
+                st.session_state.chat_history = result['chat_history']
+            response = result['answer']
+            source_documents = result['source_documents']
 
-    history = StreamlitChatMessageHistory(key="chat_messages")
+        st.session_state.messages.insert(1, {"role": "assistant", "content": response})
 
-    if query := st.chat_input("질문을 입력해주세요."):
-        st.session_state.messages.append({"role": "user", "content": query})
-
-        with st.chat_message("user"):
-            st.markdown(query)
-
-        with st.chat_message("assistant"):
-            chain = st.session_state.conversation
-
-            with st.spinner("생각 중..."):
-                result = chain({"question": query})
-                with get_openai_callback() as cb:
-                    st.session_state.chat_history = result['chat_history']
-                response = result['answer']
-                source_documents = result['source_documents']
-
-                st.markdown(response)
-                with st.expander("참고 문서 확인"):
-                    for doc in source_documents:
-                        st.markdown(doc.metadata['source'], help=doc.page_content)
-
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    for message_pair in (list(zip(st.session_state.messages[::2], st.session_state.messages[1::2]))):
+        with st.chat_message(message_pair[0]["role"]):
+            st.markdown(message_pair[0]["content"])
+        with st.chat_message(message_pair[1]["role"]):
+            st.markdown(message_pair[1]["content"])
+        with st.expander("참고 문서 확인"):
+                for doc in source_documents:
+                    st.markdown(doc.metadata['source'], help=doc.page_content)
 
 def tiktoken_len(text):
     tokenizer = tiktoken.get_encoding("cl100k_base")
@@ -100,16 +126,16 @@ def get_text_from_folder(folder_path):
     folder = Path(folder_path)
     files = folder.iterdir()
 
-    for file_path in files:
-        if file_path.is_file():
-            if file_path.suffix == '.pdf':
-                loader = PyPDFLoader(str(file_path))
+    for file in files:
+        if file.is_file():
+            if file.suffix == '.pdf':
+                loader = PyPDFLoader(str(file))
                 documents = loader.load_and_split()
-            elif file_path.suffix == '.docx':
-                loader = Docx2txtLoader(str(file_path))
+            elif file.suffix == '.docx':
+                loader = Docx2txtLoader(str(file))
                 documents = loader.load_and_split()
-            elif file_path.suffix == '.pptx':
-                loader = UnstructuredPowerPointLoader(str(file_path))
+            elif file.suffix == '.pptx':
+                loader = UnstructuredPowerPointLoader(str(file))
                 documents = loader.load_and_split()
             else:
                 documents = []
@@ -139,7 +165,7 @@ def get_conversation_chain(vectorstore, openai_api_key, model_name):
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(search_type='mmr'),
-        memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
+        memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer', input_key='question'),
         get_chat_history=lambda h: h,
         return_source_documents=True,
         verbose=True
@@ -151,10 +177,10 @@ def save_conversation_as_txt(chat_history):
     for message in chat_history:
         role = "user" if isinstance(message, HumanMessage) else "assistant"
         content = message.content
-        conversation += f"역할: {role}\n내용: {content}\n\n"
+        conversation += f"연금: {role}\n내용: {content}\n\n"
     
     b64 = base64.b64encode(conversation.encode()).decode()
-    href = f'<a href="data:file/txt;base64,{b64}" download="대화.txt">대화 다운로드</a>'
+    href = f'<a href="data:file/txt;base64,{b64}" download="대화.txt">\ub300화 다운로드</a>'
     st.markdown(href, unsafe_allow_html=True)
 
 if __name__ == '__main__':
